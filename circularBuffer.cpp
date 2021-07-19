c23898e0a29d71e2b82638a341d9c4f9719d5a84
rishabhAjay <rishabhajay24@gmail.com>
2021-07-19 11:55:11 +0530

    Added input validation

--- circularBuffer.cpp
+++ circularBuffer.cpp
@@ -3,7 +3,7 @@
 #include<string> 
 #include<cstring>
 #include<math.h>
-#include <cstdlib>
+#include <regex>
 using namespace std;
 
 void reshape(int, int);
@@ -15,25 +15,28 @@ struct elem
 };
 
 elem cqueue[5];
+
 int front = 0, rear = 0, n = 5;
 
 void init() {
 	glClearColor(0.8, 0.8, 0.8, 1.0); //set the color of the screen
 }
 
-void drawBitmap(const char* string, float x, float y) {
+void drawBitmap(void* font, const char* string, float x, float y)
+{
 	glColor3f(0.0f, 0.0f, 0.0f);
 	glRasterPos2f(x, y);
 	
 	for (int i = 0; i < strlen(string); i++)
 	{
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, string[i]);
+		glutBitmapCharacter(font, string[i]);
 	}
 }
 
 void frontAndRear(int f, int r) {
 	glFlush();
-	drawBitmap("F", 130 + f*100, 400);
+	drawBitmap(GLUT_BITMAP_HELVETICA_18 ,"F", 130 + f*100, 400);
+	glLineWidth(3);
 	glBegin(GL_LINES);
 		glVertex2f(135 + f * 100, 390);
 		glVertex2f(135 + f * 100, 320);
@@ -44,7 +47,8 @@ void frontAndRear(int f, int r) {
 		glVertex2f(135+ f * 100, 320);
 		glVertex2f(150+ f * 100, 340);
 	glEnd();
-	drawBitmap("R", 149 + r*100, 400);
+	drawBitmap(GLUT_BITMAP_HELVETICA_18, "R", 148 + r*100, 400);
+	glLineWidth(3);
 	glBegin(GL_LINES);
 		glVertex2f(155 + r * 100, 390);
 		glVertex2f(155 + r * 100, 320);
@@ -58,11 +62,20 @@ void frontAndRear(int f, int r) {
 }
 
 void insert() {
-
+	regex b("[0-9]");
 	if (front == 0 && rear == 0)
 	{
 		cout << "\nEnter the Element: ";
 		cin >> cqueue[1].c;
+	    while(true) {
+			if (!(regex_match(cqueue[1].c, b))) {
+				cout << "\nEnter a single digit Element: ";
+				cin >> cqueue[1].c;
+			}
+			else {
+				break;
+			}
+		}
 		rear = 1;
 		front = 1;
 	}
@@ -71,7 +84,7 @@ void insert() {
 		int next = (rear % n) + 1;
 		if (next == front)
 		{
-			drawBitmap("QUEUE FULL", 300, 450);
+			drawBitmap(GLUT_BITMAP_TIMES_ROMAN_24,"QUEUE OVERFLOW", 250, 470);
 			glutSwapBuffers();
 			Sleep(2000);
 			
@@ -80,6 +93,15 @@ void insert() {
 		{
 			cout << "\nEnter the Element: ";
 			cin >> cqueue[next].c;
+			while (true) {
+				if (!(regex_match(cqueue[next].c, b))) {
+					cout << "\nEnter a single digit Element: ";
+					cin >> cqueue[next].c;
+				}
+				else {
+					break;
+				}
+			}
 			rear = next;
 		}
 		
@@ -100,89 +122,95 @@ void delete1() {
 }
 
 void display() {
-	drawBitmap("1. Enter i to Push", 300, 650);
-	drawBitmap("2. Enter d to Pop", 300, 610);
-	drawBitmap("3. Enter q to Quit", 300, 570);
+	drawBitmap(GLUT_BITMAP_HELVETICA_18, "1. Enter i to Push", 325, 650);
+	drawBitmap(GLUT_BITMAP_HELVETICA_18, "2. Enter d to Pop", 325, 610);
+	drawBitmap(GLUT_BITMAP_HELVETICA_18, "3. Enter q to Quit", 325, 570);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity(); 			
-	for (int i = 0; i < 500; i += 100) {
+	glLoadIdentity(); //reset any matrix transformations
+	//clear the frame buffer first
+	//frame buffer refers to the pixels on the window			
+	for (int i = 0; i < n * 100; i += 100) {
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glColor3f(1.0, 0.5, 0.5);
 
 		glBegin(GL_POLYGON);
-			glVertex2f(200 + i, 200);
-			glVertex2f(200 + i, 300);
-			glVertex2f(300 + i, 300);
-			glVertex2f(300 + i, 200);
+		glVertex2f(200 + i, 200);
+		glVertex2f(200 + i, 300);
+		glVertex2f(300 + i, 300);
+		glVertex2f(300 + i, 200);
 		glEnd();
-		
+		glLineWidth(2);
 		glBegin(GL_LINE_LOOP);
-			glColor3f(1,1,1);
-			glVertex2f(200 + i, 200);
-			glColor3f(1, 1, 1);
-			glVertex2f(200 + i, 300);
-			glColor3f(1, 1, 1);
-			glVertex2f(300 + i, 300);
-			glColor3f(1, 1, 1);
-			glVertex2f(300 + i, 200);
-			glColor3f(1, 1, 1);
+		glColor3f(1, 1, 1);
+		glVertex2f(200 + i, 200);
+		glColor3f(1, 1, 1);
+		glVertex2f(200 + i, 300);
+		glColor3f(1, 1, 1);
+		glVertex2f(300 + i, 300);
+		glColor3f(1, 1, 1);
+		glVertex2f(300 + i, 200);
+		glColor3f(1, 1, 1);
 		glEnd();
 	}
-	
-		glBegin(GL_LINES);
-			glColor3f(0, 0, 0);
-			glVertex2f(650, 200);
-			glColor3f(0, 0, 0);
-			glVertex2f(650, 150);
-			glColor3f(0, 0, 0);
-			glVertex2f(650, 150);
-			glColor3f(0, 0, 0);
-			glVertex2f(250, 150);
-			glColor3f(0, 0, 0);
-			glVertex2f(250, 150);
-			glColor3f(0, 0, 0);
-			glVertex2f(250, 200);
-			glColor3f(0, 0, 0);
-		glEnd();
+	glLineWidth(2);
+	glBegin(GL_LINES);
+	glColor3f(0, 0, 0);
+	glVertex2f(650, 200);
+	glColor3f(0, 0, 0);
+	glVertex2f(650, 150);
+	glColor3f(0, 0, 0);
+	glVertex2f(650, 150);
+	glColor3f(0, 0, 0);
+	glVertex2f(250, 150);
+	glColor3f(0, 0, 0);
+	glVertex2f(250, 150);
+	glColor3f(0, 0, 0);
+	glVertex2f(250, 200);
+	glColor3f(0, 0, 0);
+	glEnd();
 
-		glColor3f(0, 0, 0);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glBegin(GL_POLYGON);
-			glVertex2f(225, 175);
-			glVertex2f(250, 200);
-			glVertex2f(275, 175);
-		glEnd();
-	
-		frontAndRear(front, rear);
-		if (front != 0 && rear != 0)
+	glColor3f(0, 0, 0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glBegin(GL_POLYGON);
+	glVertex2f(225, 175);
+	glVertex2f(250, 200);
+	glVertex2f(275, 175);
+	glEnd();
+	frontAndRear(front, rear);
+	if (front != 0 && rear != 0)
+	{
+
+		int i = front;
+		glColor3f(1, 1, 1);
+		glRasterPos2f(135 + ((i) * 100), (250));
+
+		for (int j = 0; j < strlen(cqueue[i].c); j++)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, cqueue[i].c[j]);
+		while (i != rear)
 		{
-			int i = front;
+			i = (i % n) + 1;
 			glColor3f(1, 1, 1);
-			glRasterPos2f(140 + ((i) * 100), (250));
+			glRasterPos2f(135 + (i * 100), (250));
 
 			for (int j = 0; j < strlen(cqueue[i].c); j++)
-					glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, cqueue[i].c[j]);
-				while (i != rear)
-				{
-					i = (i % n) + 1;
-					glColor3f(1, 1, 1);
-					glRasterPos2f(140 + (i * 100), (250));
-
-					for (int j = 0; j < strlen(cqueue[i].c); j++)
-							glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, cqueue[i].c[j]);
-				}
+				glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, cqueue[i].c[j]);
 		}
-	glutSwapBuffers(); 
+	}
+	glutSwapBuffers(); //display the updated frame buffer(the pixels)
+
 }
 
-void render() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	if (front == 0)
+
+void render() //display function 
+{
+	glClear(GL_COLOR_BUFFER_BIT); //resetting the screen
+	
+	if (front == 0 && rear == 0)
 	{
-		drawBitmap("1. Enter i to Push", 300, 650);
-		drawBitmap("2. Enter d to Pop", 300, 610);
-		drawBitmap("3. Enter q to Quit", 300,570);
-		drawBitmap("QUEUE EMPTY", 300, 450);
+		drawBitmap(GLUT_BITMAP_HELVETICA_18, "1. Enter i to Push", 325, 650);
+		drawBitmap(GLUT_BITMAP_HELVETICA_18, "2. Enter d to Pop", 325, 610);
+		drawBitmap(GLUT_BITMAP_HELVETICA_18, "3. Enter q to Quit", 325,570);
+		drawBitmap(GLUT_BITMAP_TIMES_ROMAN_24,"QUEUE UNDERFLOW", 250, 470);
 		glutSwapBuffers();
 	}
 	else {
@@ -191,14 +219,15 @@ void render() {
 	}
 }
 
-void keyboard(unsigned char key, int x, int y) {
+void keyboard(unsigned char key, int x, int y)
+{
 	switch (key)
 	{
-	case 'i' | 'I': insert(); render();
+	case 'i' : insert(); render();
 		break;
-	case 'd' | 'D':  delete1();  render();
+	case 'd' :  delete1();  render();
 		break;
-	case 'q' | 'Q': exit(0);
+	case 'q' : exit(0);
 	}
 }
 
@@ -207,15 +236,16 @@ void reshape(int w, int h) {
 	glMatrixMode(GL_PROJECTION); //change to projection matrix to make the projections
 	glLoadIdentity(); //reset any matrix transformations
 	glViewport(0, 0, w, h); //takes 4 parameters. (x,y) coordinates of the start point and the end point
-	glOrtho(0, 900, 0, 900, -100, 100); //specify the coordinate limits (check the screenshot)
+	glOrtho(0, 900, 0, 900, -100, 100); //specify the coordinate limits
 	glMatrixMode(GL_MODELVIEW); //change back to MODEL view because it is important
 	glLoadIdentity(); //load the matrix changes 
 }
 
 int main(int argc, char** argv) {
+
 	glutInit(&argc, argv); //initialise glut (boilerplate)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowPosition(500, 100); //Where will the window pop up on screen?(coordinates)
+	glutInitWindowPosition(1000, 100); //Where will the window pop up on screen?(coordinates)
 	glutInitWindowSize(500, 500); //width and height in pixels
 	glutCreateWindow("Circular Buffer"); //THis will actully create the window with the name
 	init(); //check for the error
